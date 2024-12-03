cc229a4b70cd2dc0b9632a76c92eaa64b7450f21
Harvey Doan <mnhatlinh.doan@gmail.com>
2024-12-03 08:20:50 +0700

    feat: update 03 Dec 2024

--- i3/toggle_floating.sh
+++ i3/toggle_floating.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-# Define the flag file location
-FLAG_FILE="/tmp/i3_floating_state"
+# Get current workspace
+workspace=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused).name')
 
-# Get the name of the currently focused workspace
-workspace=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true).name')
-
-# Check if any window in the current workspace is currently floating
-is_any_floating=$(i3-msg -t get_tree | jq -r --arg workspace "$workspace" '.nodes[].nodes[] | select(.name==$workspace) | .nodes[] | select(.floating=="user_on")' | wc -l)
-
-# If any window is floating, disable floating for all windows and remove the flag file
-if [ "$is_any_floating" -gt 0 ]; then
-    i3-msg "[workspace=$workspace] floating disable"
-    rm -f "$FLAG_FILE"
+# Add or remove floating rules for the workspace
+if grep -q "$workspace" ~/.config/i3/floating_workspaces; then
+    sed -i "/$workspace/d" ~/.config/i3/floating_workspaces
+    i3-msg "floating disable"
 else
-    # If no window is floating, enable floating for all windows and create a flag file
-    i3-msg "[workspace=$workspace] floating enable"
-    echo "$workspace" > "$FLAG_FILE"
+    echo "$workspace" >> ~/.config/i3/floating_workspaces
+    i3-msg "floating enable"
 fi
 
--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,10 +4,11 @@
 # If all your bars have ipc enabled, you can use
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
-# killall -q polybar
+killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar linhdmn 2>&1 | tee -a /tmp/polybar1.log & disown
+# polybar -r default -c $HOME/.config/polybar/rice.ini
 
 echo "Bars launched..."
