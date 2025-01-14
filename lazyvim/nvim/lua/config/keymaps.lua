6c58894a078c2effa9c8f7f0f7b90bcc74ca1a3a
Linh Doan <linh.doan@bluesg.com.sg>
2025-01-14 15:15:26 +0700

    feat: setup needed configs for lazyvim

--- lazyvim/nvim/lua/config/keymaps.lua
+++ lazyvim/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- -- In visual mode, move selected lines up with K, down with J
+vim.keymap.set("x", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set("x", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
