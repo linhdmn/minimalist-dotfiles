86c80bd4cba2331cc2c1d7e13e91f8c19cde98b8
Harvey Doan <mnhatlinh.doan@gmail.com>
2025-03-02 11:28:18 +0700

    feat: update lazyvim

--- lazyvim/nvim/lua/config/options.lua
+++ lazyvim/nvim/lua/config/options.lua
@@ -7,3 +7,13 @@ vim.opt.tabstop = 4 -- Number of visual spaces per TAB
 vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
 vim.opt.expandtab = true -- Convert tabs to spaces
 vim.opt.smartindent = true -- Enable smart indentation
+vim.opt.colorcolumn = "120"
+
+-- Set transparent background for both active and inactive windows
+vim.api.nvim_create_autocmd({ "VimEnter", "WinEnter", "BufWinEnter" }, {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+  end,
+})
